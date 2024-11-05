a00086d48ffd5b7d08da90b7009a4475a6f2707d
vis1xn <g00421432@atu.ie>
2024-11-05 10:08:15 +0000

    Started Lab exam

---
+++ src/main/java/ie/atu/week7_lab_exam/Week7LabExamApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week7_lab_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week7LabExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week7LabExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week7_lab_exam/Week7LabExamApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week7_lab_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week7LabExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
