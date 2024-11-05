6bf55798b857e408365f6d9909753ec9aedb8dbb
vis1xn <g00421432@atu.ie>
2024-11-05 10:48:01 +0000

    added code to ControllerAdvice and Product

--- src/main/java/ie/atu/week7_lab_exam/Product.java
+++ src/main/java/ie/atu/week7_lab_exam/Product.java
@@ -1,1 +1,28 @@
 package ie.atu.week7_lab_exam;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @NotBlank(message = "Code cannot be blank")
+    private String code;
+
+    @NotBlank(message = "Category cannot be blank")
+    private String category;
+
+    @PositiveOrZero(message = "Quantity must be greater than or equal to zero")
+    private int quantity;
+
+    @PositiveOrZero(message = "Price cannot be below zero")
+    private double price;
+
+}
